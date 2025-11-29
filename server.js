0f5b29e0834714a1abe747f3033f088192471cc5
Mustafa <mustafakank19@gmail.com>
2025-11-29 05:00:44 +0300

    ai hatası dzüeltildi

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ require('dotenv').config();
 // GEMINI AYARLARI
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
 const app = express();
 const port = 3000;
